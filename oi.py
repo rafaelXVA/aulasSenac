ac1b0bc0028edc3682fabaaee10657312352a69b
rafaelXVA <rafael.rocha2745@gmail.com>
2024-09-23 18:03:03 -0300

    outro

--- oi.py
+++
@@ -1,6 +0,0 @@
-sal=int(input("digite salário\n"))
-desc=4.6
-salarioR=sal*desc/100
-salarioR=sal-salarioR
-mensagem="LOL"
-print("seu salário é de %d porém recebe %.2f de descontos que resulta em %.2f reais\nmensagem do chefe %s"%(sal,desc,salarioR,mensagem))
