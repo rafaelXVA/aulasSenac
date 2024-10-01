111f1ac0f746e3c62d52184d1b871ff8fe68896b
rafaelXVA <rafael.rocha2745@gmail.com>
2024-10-01 18:32:04 -0300

    01/10/24

---
+++ exc1-01-10-24.py
@@ -0,0 +1,6 @@
+a=[1,2,3,4,5]
+print(max(a))
+print(min(a))
+print(len(a))
+print(sum(a))
+print(a[:])
---
+++ lista.py
@@ -0,0 +1,44 @@
+"""lista=["americaYa",90,1.9,"oi"]
+lista2=["HAHAHAHAHAAHAH","im becoming crazy fellas","yoink"]
+print (lista,lista2)
+#str, int, float, str
+
+#lista dentro de lista
+
+lista3=["im dying sister","ok",["i got TB, beating a man, for a few bucks","but you always helping people"]]
+print(lista3)
+
+
+print(lista[0])
+#detalhar mais o print
+
+print(len(lista))
+print(len(lista2))
+print(len(lista3))
+#quantos elementos tem
+
+america=["hallo","ya","chiyo","osaka",["Oi"]]
+
+print("Oi" in america,"\n")
+print([1,2]+[3,4],"\n")
+print(america + [1,2],"\n")
+print(america*2)
+print([1,2,["oi" , "hallo" in america]]*2)
+
+a=[1,2,3,4,5,6,7,8,9]
+print(a)
+print(min(a))
+print(max(a))
+print(sum(a))
+
+a=["alpha","aatrox","beta","color","delta","dino"]
+print(max(a))
+print(min(a))"""
+
+#[1:3] não entra no três, ou seja, para no 2
+list=["a",'b','c','d','e','f']
+print(list[1:3])
+print(list[:4])
+print(list[3:])
+print(list[:])
+print(list[0:6])
