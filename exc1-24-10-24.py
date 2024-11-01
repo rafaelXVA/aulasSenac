bff67d1cd77dfcd4db6180bb1a7ace99aae7f87a
rafaelXVA <rafael.rocha2745@gmail.com>
2024-11-01 15:44:47 -0200

    AAA

---
+++ exc1-24-10-24.py
@@ -0,0 +1,4 @@
+for x in range(1,11):
+    for y in range(1,11):
+        z=x*y
+        print(x,'x',y,'=',z)
---
+++ for.py
@@ -0,0 +1,47 @@
+'''
+    //imprime cada elemento//
+
+oi=['america','ya','hallo']
+
+for sabe_muito in oi:
+    print(sabe_muito)
+
+    //imprime cada letra//
+
+seesh='coe mano'
+
+for sapato in seesh:
+    print(sapato)
+
+    //para quando chegar no ya//
+
+oi=['america','ya','hallo']
+
+for seesh in oi:
+    print(seesh)
+    if seesh=='ya':
+        break  
+
+    //pula um elemento e continua o loop//
+
+coe=['sapato','honda civic 2006','airam capuani']
+
+for oi in coe:
+    if oi=='honda civic 2006':
+        continue
+    print(oi) 
+
+    //iniciar em um ponto//
+
+for x in range(1,11):
+    print(x) 
+
+    //pular números
+
+for x in range(5,101,5):
+    print(x) '''
+
+
+for x in range(1,4):
+    for y in range(1,6):
+        print(x,y)
