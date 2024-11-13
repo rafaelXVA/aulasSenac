37fdf0b350980b72c2d81f37804e4fa32d9fa1d0
rafaelXVA <rafael.rocha2745@gmail.com>
2024-11-13 00:59:20 -0400

    lista 2

---
+++ lista 2/exc1.py
@@ -0,0 +1,10 @@
+def Mult(lista, mult):
+    for x in lista:
+        x*mult
+    return [lista]
+
+list=[2,3,4,2,1,4]
+x=2
+print(Mult(list,x))
+
+
---
+++ lista 2/exc2.py
@@ -0,0 +1,7 @@
+def Mult(lista):
+    for x in lista:
+        print(x)
+        x*3
+        return lista
+lista=[2,3,4,5,6]
+print(Mult(lista))
